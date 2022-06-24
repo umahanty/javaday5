2083ebf023c1fe7a177ffd801d5bf8a3564cbed6
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-24 13:13:51 +0530

    Add files via upload

---
+++ largestofthree.sh
@@ -0,0 +1,18 @@
+
+public class largestofthree {
+
+	public static void main(String[] args) {
+		int a =20;
+		int b =10;
+		int c =30;
+		
+	if(c >= b  &&  c >=a)
+		System.out.println(c+" is the largest number");
+	else if(a >=b  && a >=c)
+		System.out.println(b+ "is the lagest number");
+	else
+		System.out.println(c+ "is smallest number");
+		
+	}
+
+}
