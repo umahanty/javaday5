a9454eca5106b30d35c9170d5e88192cf551eb37
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-24 19:04:43 +0530

    Add files via upload

---
+++ quotientandreminder.sh
@@ -0,0 +1,14 @@
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Divident: ");
+		int dividend = sc.nextInt();
+		System.out.println("Enter Divisor: ");
+		  int divisor = sc.nextInt();
+		   int quotient = dividend / divisor;
+		  int remainder = dividend % divisor;
+
+			    System.out.println("Quotient = " + quotient);
+			    System.out.println("Remainder = " + remainder);
+			  }
+	}
