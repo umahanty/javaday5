8bf69097217bb6f2c1c57237645264715eec3c85
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-25 10:20:09 +0530

    Update leapyear.sh

--- leapyear.sh
+++ leapyear.sh
@@ -12,9 +12,9 @@ public static void main(String[] args) {
 	      year = sc.nextInt();
 
 	      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-	         System.out.println("Specified year is a leap year");
+	         System.out.println("The  year is a leap year..");
 	      
 	      else
-	         System.out.println("Specified year is not a leap year");
+	         System.out.println("The  year is not a leap year..");
 	   }
 	}
